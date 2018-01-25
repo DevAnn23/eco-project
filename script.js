d4b322b702829f9a7e6c26e05628c065ef401423
DevAnn23 <abanaszek2@gmail.com>
2018-01-25 14:17:02 +0100

    Vue onclick

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 //'use strict';
 jQuery
-$('input[type=range]').on('input', function(e){
-  var min = e.target.min,
-      max = e.target.max,
-      val = e.target.value;
+$('input[type=range]').on('input', function (e) {
+	var min = e.target.min,
+		max = e.target.max,
+		val = e.target.value;
 
-  $(e.target).css({
-    'backgroundSize': (val - min) * 100 / (max - min) + '% 100%'
-  });
+	$(e.target).css({
+		'backgroundSize': (val - min) * 100 / (max - min) + '% 100%'
+	});
 }).trigger('input');
 
-window.onload = function(){
-var input = document.querySelector("input"),
-	night = document.querySelector(".night"),
-	valueOpacity = document.querySelector(".valueOpacity");
-night.style.opacity = input.value / 100;
+window.onload = function () {
+	var input = document.querySelector("input"),
+		night = document.querySelector(".night"),
+		valueOpacity = document.querySelector(".valueOpacity");
+	night.style.opacity = input.value / 100;
 
-function updateValue() {
-	night.style.opacity = this.value / 100;
-	valueOpacity.innerHTML = this.value + "%";
-}
+	function updateValue() {
+		night.style.opacity = this.value / 100;
+		valueOpacity.innerHTML = this.value + "%";
+	}
 
-input.addEventListener('change', updateValue);
-input.addEventListener('mousemove', updateValue);
+	input.addEventListener('change', updateValue);
+	input.addEventListener('mousemove', updateValue);
 
 };
 
@@ -32,6 +32,7 @@ input.addEventListener('mousemove', updateValue);
 var app = new Vue({
 	el: '#app',
 	data: {
+		categories: ['Frontend', 'Backend', 'Marketing', 'Po godzinach'],
 		message: ''
 	}
 })
@@ -40,7 +41,6 @@ var app2 = new Vue({
 	el: '#app-2',
 	data: {
 		message: 'You loaded this page on ' + new Date().toLocaleString(),
-
 	}
 })
 
@@ -51,6 +51,38 @@ var app3 = new Vue({
 
 	}
 })
+var app4 = new Vue({
+	el: '#app4',
+	data: {
+		categories: ['Frontend', 'Backend', 'Marketing', 'Po godzinach']
+	}
+})
+var app8 = new Vue({
+  el: '#app8',
+  data: {
+    counter: 0,
+    categories: ['Frontend', 'Backend']
+  },
+  methods: {
+    addCategory() {
+		if(!(this.category === '')){
+		this.categories.push(this.category);
+		}
+		else{
+			alert("Pole nie może byc puste")
+		}
+      this.category = '';
+    }
+  }
+})
+function addTask(){
+	var inputTask = document.querySelector(".inputTask"),
+		submitTask = document.querySelector(".submitTask");
+	app4.categories.push( 'New Task');
+	alert(app5.counter);
+}
+
+
 
 // Social media
 var yScrollAxis = Math.floor(window.pageYOffset);
